6dddee9cb50c8cd81e48afd47e40f05a3d67b8e1
David Clark <david.dave.clark@gmail.com>
2013-12-29 14:22:54 -0700

    Code opens and closes

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ $('#btn-download').click(function() {
   $('#hidden-download').cssAnimateAuto();
   $('#hidden-1').cssAnimateAuto('close');
 });
+
+$('#see-code-accordion').click(function() {
+  $('#code-accordion').cssAnimateAuto();
+});
+
+$('#see-code-dd').click(function() {
+  $('#code-dd').cssAnimateAuto();
+});
