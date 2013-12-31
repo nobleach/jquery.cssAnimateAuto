52cd14ecd1f0f8266492ff256b324cc39c2c659b
David Clark <david.dave.clark@gmail.com>
2013-12-30 20:45:33 -0700

    Refinement of example page

---
+++ _includes/basic.js
@@ -0,0 +1,6 @@
+$('#basic-height-btn').click(function() {
+  $('#basic-height-content').cssAnimateAuto();
+});
+$('#basic-width-btn').click(function() {
+  $('#basic-width-content').cssAnimateAuto('width 0.3s');
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-$('#btn-1').click(function() {
-  $('#hidden-1').cssAnimateAuto();
-  $('#hidden-download').cssAnimateAuto('close');
-});
-
 $('#btn-download').click(function() {
   $('#hidden-download').cssAnimateAuto();
   $('#hidden-1').cssAnimateAuto('close');
